3581aca45c5034062f789bb4d41a560e7ad12966
sebastian g <prototipokrak@gmail.com>
2024-08-03 10:17:59 -0500

    GA7-220501096-AA4-EV03

---
+++ src/products.js
@@ -0,0 +1,74 @@
+import imageProduct1 from './assets/images/1.png'
+import imageProduct2 from './assets/images/2.png'
+import imageProduct3 from './assets/images/3.png'
+import imageProduct4 from './assets/images/4.png'
+import imageProduct5 from './assets/images/5.png'
+import imageProduct6 from './assets/images/6.png'
+import imageProduct7 from './assets/images/7.png'
+import imageProduct8 from './assets/images/8.png'
+export const products = [
+    {
+        id: 1,
+        name: 'Silla Tapizada Hertfoid',
+        price: 101,
+        image: imageProduct1,
+        description: 'Animación mollit consectetur, adición aute, pariatur ad mollit. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor culpa Lorem ipsum ullamco dolor sit do. Proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'silla-tapizada-hertfoid'
+    },
+    {
+        id: 2,
+        name: 'Silla Giratoria Tapizada Abingdon',
+        price: 151,
+        image: imageProduct2,
+        description: 'Animación mollit consectetur, adición aute, pariatur ad mollit. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor culpa Lorem ipsum ullamco dolor sit do. Proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'silla-giratoria-tapizada-abingdon'
+    },
+    {
+        id: 3,
+        name: 'Jeses Minimore Estilo Moderno Etta',
+        price: 181,
+        image: imageProduct3,
+        description: 'Animación mollit consectetur, adición aute, pariatur ad mollit. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor culpa Lorem ipsum ullamco dolor sit do. Proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'jeses-minimore-estilo-moderno-etta'
+    },
+    {
+        id: 4,
+        name: 'JJesús Minimore Estilo Moderno',
+        price: 201,
+        image: imageProduct4,
+        description: 'Animación mollit consectetur, adición aute, pariatur ad mollit. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor culpa Lorem ipsum ullamco dolor sit do. Proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'jesus-minimore-estilo-moderno'
+    },
+    {
+        id: 5,
+        name: 'Sillón Tapizado Bolanle',
+        price: 251,
+        image: imageProduct5,
+        description: 'Animación mollit consectetur, adición aute, pariatur ad mollit. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor culpa Lorem ipsum ullamco dolor sit do. Proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'sillon-tapizado-bolanle'
+    },
+    {
+        id: 6,
+        name: 'Sillón Tapizado Jaqueze',
+        price: 111,
+        image: imageProduct6,
+        description: 'Animación mollit consectetur, adición aute, pariatur ad mollit. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor culpa Lorem ipsum ullamco dolor sit do. Proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'sillon-tapizado-jaqueze'
+    },
+    {
+        id: 7,
+        name: 'Tela Tapizada Ancha Leston',
+        price: 121,
+        image: imageProduct7,
+        description: 'Animación mollit consectetur, adición aute, pariatur ad mollit. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor culpa Lorem ipsum ullamco dolor sit do. Proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'tela-tapizada-ancha-leston'
+    },
+    {
+        id: 8,
+        name: 'Sillón Acolchado Stephanny de 27.5"',
+        price: 220,
+        image: imageProduct8,
+        description: 'Animación mollit consectetur, adición aute, pariatur ad mollit. Officia amet aliqua non laborum dolore sint sit eu sit sunt laboris. Dolor tempor culpa Lorem ipsum ullamco dolor sit do. Proident quis ad laborum et proident laborum. Non voluptate eiusmod aute labore ea aute.',
+        slug: 'sillon-acolchado-stephanny-27-5'
+    },
+];
